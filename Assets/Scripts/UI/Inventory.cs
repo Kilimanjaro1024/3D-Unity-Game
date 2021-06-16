39b0220487b96ef4090a6fa318c6bcd5b187f012
Clayton Miller <clayton.miller@loop.colum.edu>
2021-06-16 17:57:39 -0500

    Links Inventory functionality to Inventory UI.

--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -6,30 +6,29 @@ public class Inventory : MonoBehaviour
 {
     public List<Item> characterItems = new List<Item>();
     public ItemDatabase itemDatabase;
+    public UIInventory inventoryUI;
 
     void Start(){
         
         GiveItem(1);
         GiveItem(0);
         GiveItem(2);
-        RemoveItem(2);
-        RemoveItem(0);
-        for (int i = 0; i < characterItems.Count; i++)
-        {
-            Debug.Log(characterItems[i].title);
-            Debug.Log(characterItems[i].id);
-        }
+        // RemoveItem(2);
+        // RemoveItem(0);
+        
     }
 
     public void GiveItem(int id){
         Item itemToAdd = itemDatabase.GetItem(id);
         characterItems.Add(itemToAdd);
+        inventoryUI.AddNewItem(itemToAdd);
         Debug.Log("Added item: " + itemToAdd.title);
     }
 
     public void GiveItem(string itemName){
         Item itemToAdd = itemDatabase.GetItem(itemName);
         characterItems.Add(itemToAdd);
+        inventoryUI.AddNewItem(itemToAdd);
         Debug.Log("Added item: " + itemToAdd.title);
     }
 
@@ -44,7 +43,7 @@ public class Inventory : MonoBehaviour
         if (itemToRemove != null)
         {
             characterItems.Remove(itemToRemove);
-            
+            inventoryUI.RemoveItem(itemToRemove);
             Debug.Log("Removed item: " + itemToRemove.title);
         }
     }
