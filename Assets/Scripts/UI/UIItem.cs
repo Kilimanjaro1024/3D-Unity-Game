6a04f5497443c9ef0c1abd6bcc24e22054bc0c36
Clayton Miller <clayton.miller@loop.colum.edu>
2021-06-19 10:36:55 -0500

    Adds the ability to remove an item from your inventory from the inventory screen

--- Assets/Scripts/UI/UIItem.cs
+++ Assets/Scripts/UI/UIItem.cs
@@ -10,13 +10,14 @@ public class UIItem : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler,
     private Image spriteImage;
     private UIItem selectedItem;
     private Tooltip tooltip;
+    public Inventory inventory;
 
-    private void Awake()
-    {
+    private void Awake(){
         spriteImage = GetComponent<Image>();
         UpdateItem(null);
         selectedItem = GameObject.Find("SelectedItem").GetComponent<UIItem>();
         tooltip = GameObject.Find("Tooltip").GetComponent<Tooltip>();
+        inventory = GameObject.Find("Player").GetComponent<Inventory>();
     }
 
     public void UpdateItem(Item item){
@@ -32,32 +33,42 @@ public class UIItem : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler,
     }
 
     public void OnPointerClick(PointerEventData eventData){
-        if(this.item != null){
-            if(selectedItem.item != null){
-                Item clone = new Item(selectedItem.item);
-                selectedItem.UpdateItem(this.item);
-                UpdateItem(clone);
+        //Pick up and move an item in the inventory
+        if(eventData.button == PointerEventData.InputButton.Left){
+            if(this.item != null){
+                if(selectedItem.item != null){
+                    Item clone = new Item(selectedItem.item);
+                    selectedItem.UpdateItem(this.item);
+                    UpdateItem(clone);
+                }
+                else{
+                    selectedItem.UpdateItem(this.item);
+                    UpdateItem(null);
+                }
             }
-            else{
-                selectedItem.UpdateItem(this.item);
-                UpdateItem(null);
+            else if(selectedItem.item != null){
+                UpdateItem(selectedItem.item);
+                selectedItem.UpdateItem(null);
+            }    
+        }
+
+        //Remove an Item From the Iventory Menu
+        if(eventData.button == PointerEventData.InputButton.Right){
+             if(this.item != null){
+                Debug.Log("Remove Item");
+                inventory.RemoveItem(item.id);
             }
         }
-        else if(selectedItem.item != null){
-            UpdateItem(selectedItem.item);
-            selectedItem.UpdateItem(null);
-        }
-        // throw new System.NotImplementedException();
     }
 
     public void OnPointerEnter(PointerEventData eventData){
-        if(this.item != null)
-        {
+        if(this.item != null){
             tooltip.GenerateTooltip(this.item);
         }
+        
     }
-
     public void OnPointerExit(PointerEventData eventData){
         tooltip.gameObject.SetActive(false);
     }
+   
 }
