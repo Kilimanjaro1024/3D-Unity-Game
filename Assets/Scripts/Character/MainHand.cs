0b95652d6c535e5c7b61bcd5db90bf7eab7c7334
Clayton Miller <clayton.miller@loop.colum.edu>
2021-06-25 16:17:41 -0500

    Adds strong strikes and quick strikes.

--- Assets/Scripts/Character/MainHand.cs
+++ Assets/Scripts/Character/MainHand.cs
@@ -5,19 +5,49 @@ using UnityEngine;
 public class MainHand : MonoBehaviour
 {
     public Animator Anim;
+    public float timer;
+    public bool strong;
     // Start is called before the first frame update
     // Update is called once per frame
     void Update()
     {
-        QuickAttack();
+        HandleAttack();
     }
 
-    public void QuickAttack(){
-        if(Input.GetButtonDown("Fire1")){
-            Anim.SetBool("swing", true);   
+    private void HandleAttack()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            timer += Time.deltaTime;
+            if(timer >= 1){
+                strong = true;
+                Anim.SetBool("strong", true);
+            }
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            if (strong) {
+                StrongAttack();
+                Debug.Log("STRONG");
+            }
+            else {
+                QuickAttack();
+            }
+            // Anim.SetBool("swing", false);
+            timer = 0;
         }
         else{
             Anim.SetBool("swing", false);
         }
+    }
+
+    public void QuickAttack(){
+        Debug.Log("QUICK");
+        Anim.SetBool("swing", true);  
+    }
+
+    public void StrongAttack(){
+        Anim.SetBool("swing", true); 
+        Anim.SetBool("strong", false); 
     }
 }
