b13a7163043fa08efe96d74fbea2a9140777af3b
Clayton Miller <clayton.miller@loop.colum.edu>
2021-06-27 16:58:51 -0500

    Adds a moveToTarget function that will have the Npc target and follow the player. Also adds a function to change the state of the AI.

--- Assets/Scripts/NPCs/AI/NPCAIBasic.cs
+++ Assets/Scripts/NPCs/AI/NPCAIBasic.cs
@@ -11,8 +11,14 @@ public class NPCAIBasic : MonoBehaviour
     private NavMeshAgent agent;
     private int destNum;
     private AIState curState;
+    private float viewDistance = 10.0f;
+    private RaycastHit hit;
+    private bool rayHit;
+    private Transform target;
+    private float combatRange = 5f;
+    public bool alerted = false;
 
-    public bool alerted;
+    
     void Start () {
         destNum = 0;
         agent = GetComponent<NavMeshAgent>();
@@ -21,10 +27,12 @@ public class NPCAIBasic : MonoBehaviour
     }
 
     void Update(){
+        UpdateState();
         StateMachine();
+        FieldOfVision();
     }
 
-    public void StateMachine(){
+    private void StateMachine(){
         switch(curState){
             case AIState.Patrol:
                 Patrol();
@@ -34,17 +42,45 @@ public class NPCAIBasic : MonoBehaviour
             case AIState.Combat:
                 break;
             case AIState.MovingToTarget:
+                MoveToTarget();
                 break;
         }
     }
 
-    public void Patrol(){
+    private void UpdateState(){
+        if(alerted){
+            Debug.Log("Alerted");
+            curState = AIState.MovingToTarget;
+        }
+    }
+
+    private void Patrol(){
         if(agent.remainingDistance < 0.5f){
             if(destinations.Count  == 0){
                 return;
             }
             agent.destination = destinations[destNum].position;
             destNum = (destNum + 1) % destinations.Count;
+        }
+    }
+    
+    private void MoveToTarget(){
+        agent.destination = target.position;
+        if(agent.remainingDistance <= combatRange){
+            curState = AIState.Combat;
+        }
+
+    }
+
+    private void FieldOfVision(){
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        rayHit = Physics.Raycast(gameObject.transform.position, forward, out hit, viewDistance);
+        Debug.DrawRay(gameObject.transform.position, forward * viewDistance, Color.green);
+        if(hit.transform != null && hit.transform.tag == "Player"){
+            target = hit.transform;
+            if(target != gameObject.transform){
+                alerted = true;
+            }
         }
     }
 }
